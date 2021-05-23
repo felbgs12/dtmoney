c719f1d218996a6dbe5c88c417d7839f45b0fd34
Felipe Borges <felbgs12@gmail.com>
2021-05-23 14:25:33 -0300

    feat: new component NewTransactionModal

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Modal from 'react-modal';
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 import { GlobalStyle } from './styles/global';
 
@@ -22,13 +23,14 @@ export function App() {
   return (
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
       <Dashboard />
-      <Modal
+
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Register transaction</h2>
-      </Modal>
+      />
+
       <GlobalStyle />
     </>
   );
