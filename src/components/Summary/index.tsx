37e936ed9c939ed3829314c8af97197afd72af15
Felipe Borges <felbgs12@gmail.com>
2021-05-31 21:36:06 -0300

    feat: adds simples context

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,16 +1,20 @@
+import { useContext } from 'react';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import totalImg from '../../assets/total.svg';
+import { TransactionsContext } from '../../TransactionsContext';
 
 import { Container } from './styles';
 
 export function Summary() {
+  const data = useContext(TransactionsContext);
+
   return (
     <Container>
       <div>
         <header>
           <p>Incomes</p>
-          <img src={incomeImg} alt='Entradas' />
+          <img src={incomeImg} alt="Entradas" />
         </header>
         <strong>U$ 250,00</strong>
       </div>
@@ -18,15 +22,15 @@ export function Summary() {
       <div>
         <header>
           <p>Outcomes</p>
-          <img src={outcomeImg} alt='Saídas' />
+          <img src={outcomeImg} alt="Saídas" />
         </header>
         <strong>U$ 150,00</strong>
       </div>
 
-      <div className='highlight-background'>
+      <div className="highlight-background">
         <header>
           <p>Total</p>
-          <img src={totalImg} alt='Total' />
+          <img src={totalImg} alt="Total" />
         </header>
         <strong>U$ 100,00</strong>
       </div>
